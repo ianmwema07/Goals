d179aa25cc7f9481c26b74adc9d7cb345e5e3bf7
iano <ianmwema07@gmail.com>
2022-04-11 16:56:45 +0300

    add changes

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Goal } from '../goal';
 import { GoalService } from '../goal-service/goal.service';
 import { AlertService } from '../alert-service/alert.service';
+import { Quote } from '../quote';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-goal',
@@ -11,6 +13,7 @@ import { AlertService } from '../alert-service/alert.service';
 export class GoalComponent implements OnInit {
   goals: Goal[] = [];
   alertService:AlertService;
+  quote!: Quote;
 
   addNewGoal(goal: Goal){
     let goalLength = this.goals.length;
@@ -43,12 +46,23 @@ export class GoalComponent implements OnInit {
   }
 
 
-  constructor(goalService:GoalService, alertService:AlertService) {
+  constructor(goalService:GoalService, alertService:AlertService, private http:HttpClient) {
     this.goals = goalService.getGoals()
     this.alertService = alertService;
    }
 
   ngOnInit(): void {
+
+  interface ApiResponse{
+    author:string;
+    quote:string;
   }
+
+  this.http.get<ApiResponse>("http://quotes.stormconsultancy.co.uk/random.json").subscribe(data=>{
+      // Succesful API request
+      this.quote = new Quote(data.author, data.quote)
+    })
+  }
+
 
 }
